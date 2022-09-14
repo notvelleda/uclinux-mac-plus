2e7bd500d8206a7f29fde8a0ebd39587627a6c44
notvelleda <pugduddly@gmail.com>
2022-09-14 17:17:20 -0500

    oops 2: electric boogaloo

--- uClinux/buildindocker.sh
+++ uClinux/buildindocker.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-make dep && make linux-2.0.x modules
+make dep && make subdirs modules
 m68k-elf-objcopy -O binary -j .text /linux/linux-2.0.x/linux /linux/images/linux.text
 m68k-elf-objcopy -O binary -j .data /linux/linux-2.0.x/linux /linux/images/linux.data
 cat images/linux.text images/linux.data > images/kernel.img
